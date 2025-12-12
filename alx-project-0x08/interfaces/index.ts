b6b76037d2ba9142e10e1fbb6481e88421fe0e64
pius wanyangu <ndubipius96@gmail.com>
2025-12-12 10:39:24 +0300

    added application of state

---
+++ alx-project-0x08/interfaces/index.ts
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export interface ReactComponentProps {
+    children: React.ReactNode
+}
+
+// image generator interface
+export interface GeneratedImageProps {
+    imageUrl: string
+    prompt: string
+    width?: string
+    height?: string
+    action: (imagePath: string) => void
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
